efc64ef2e7b2c179d5ad719b58a9024c7db36eda
Zach Riane Machacon <zachriane01@gmail.com>
2023-05-24 00:00:23 +0800

    [CHORE] Removed dummy data and added typesafety to data from backend

--- client/src/layouts/TodoWindow.tsx
+++ client/src/layouts/TodoWindow.tsx
@@ -3,27 +3,6 @@ import { AddTodoButton } from "../components/AddTodoButton";
 import { TodoList } from "../components/TodoList";
 import { Todo } from "../globals/types";
 
-// const todos: Todo[] = [
-//   {
-//     message: "Practice React with TypeScript",
-//     completed: false,
-//     time_updated: new Date("2023-05-19"),
-//     time_created: new Date("2023-05-19"),
-//   },
-//   {
-//     message: "Dance Cupid",
-//     completed: false,
-//     time_updated: new Date("2023-04-25"),
-//     time_created: new Date("2023-04-19"),
-//   },
-//   {
-//     message: "Do homework",
-//     completed: false,
-//     time_updated: new Date("2023-05-20"),
-//     time_created: new Date("2023-05-19"),
-//   },
-// ];
-
 export const TodoWindow = () => {
   const [todos, setTodos] = useState<Todo[]>([{
     message: "",
@@ -33,9 +12,8 @@ export const TodoWindow = () => {
   }])
   useEffect(() => {
     const getTodos = async () => {
-      const response = await fetch('http://127.0.0.1:8000/api/todo/');
-      const todoData = await response.json();
-      console.log(todoData)
+      const response: Response = await fetch('http://127.0.0.1:8000/api/todo/');
+      const todoData: Todo[] = await response.json();
       setTodos(todoData);
     }
     getTodos();
